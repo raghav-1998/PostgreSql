cc429e7433fcb23e3a746acfd4ade39e52792403
Raghav Vasdeva <raghav.vasdeva@gmail.com>
2024-09-17 17:23:54 +0530

    Exploration of DvdRental Dataset

---
+++ TablesExploration.sql
@@ -0,0 +1,59 @@
+Select column_name, data_type 
+from information_schema.columns
+where table_name='actor';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='address';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='category';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='city';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='country';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='customer';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='film';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='film_actor';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='film_category';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='inventory';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='language';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='payment';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='rental';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='staff';
+
+Select column_name, data_type 
+from information_schema.columns
+where table_name='store';
